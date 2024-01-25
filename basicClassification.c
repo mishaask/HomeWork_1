25df61f99fdd0dc09f6ab492a6aa0da07560e2cb
mishaask <122132689+mishaask@users.noreply.github.com>
2024-01-25 05:11:07 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -4,7 +4,8 @@
 int factorial(int n);
 
 int isPrime(int num){
-    if(num<=1){return 0;}
+    if(num < 1){return 0;}
+    if(num == 1){return 1;}
 
     for(int i = 2; i*i<=num;++i){
         if(num % i==0){
