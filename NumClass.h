12f03615d80f38e6c0947b746e2158da2527492a
mishaask <122132689+mishaask@users.noreply.github.com>
2024-01-25 03:25:39 +0200

    Numclass.h

---
+++ NumClass.h
@@ -0,0 +1,16 @@
+#ifndef NUMCLASS_H
+#define NUMCLASS_H
+
+int isArmstrong(int num);
+int isPalindrome(int num);
+int isPrime(int num);
+int isStrong(int num);
+int factorial(int n);
+int reverseNumberL(int num);
+int reverseNumberR(int num, int reversed);
+int countDigitsL(int num);
+int countDigitsR(int num);
+int calculateArmstrongSum(int num, int n);
+int power(int base, int exponent);
+
+#endif /* NUMCLASS_H */
